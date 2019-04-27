91e892ca7c61b03f4041f7ab6311f5bfcda4df34
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-04-26 23:29:03 -0700

    add modal popup & frontend validation

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -2,57 +2,82 @@ $(document).ready(function() {
   $('.my-select').selectpicker('render');
 });
 
+var validateFormData = function() {
+  var isFormValid = true;
+
+  $('.input-form').each(function() {
+    if ($(this).val() === '') {
+      isFormValid = false;
+    }
+  });
+
+  $('.selectpicker').each(function() {
+    if ($(this).val() == 0) {
+      isFormValid = false;
+    }
+  });
+
+  return isFormValid;
+};
+
 $('#submit-survey').on('click', function(event) {
   event.preventDefault();
 
-  var dataForm = {
-    name: $('#name')
-      .val()
-      .trim(),
-    photoLink: $('#photo-link')
-      .val()
-      .trim(),
-    scores: [
-      $('#q1')
+  if (validateFormData()) {
+    var dataForm = {
+      name: $('#name')
         .val()
         .trim(),
-      $('#q2')
+      photoLink: $('#photo-link')
         .val()
         .trim(),
-      $('#q3')
-        .val()
-        .trim(),
-      $('#q4')
-        .val()
-        .trim(),
-      $('#q5')
-        .val()
-        .trim(),
-      $('#q6')
-        .val()
-        .trim(),
-      $('#q7')
-        .val()
-        .trim(),
-      $('#q8')
-        .val()
-        .trim(),
-      $('#q9')
-        .val()
-        .trim(),
-      $('#q10')
-        .val()
-        .trim()
-    ]
-  };
+      scores: [
+        $('#q1')
+          .val()
+          .trim(),
+        $('#q2')
+          .val()
+          .trim(),
+        $('#q3')
+          .val()
+          .trim(),
+        $('#q4')
+          .val()
+          .trim(),
+        $('#q5')
+          .val()
+          .trim(),
+        $('#q6')
+          .val()
+          .trim(),
+        $('#q7')
+          .val()
+          .trim(),
+        $('#q8')
+          .val()
+          .trim(),
+        $('#q9')
+          .val()
+          .trim(),
+        $('#q10')
+          .val()
+          .trim()
+      ]
+    };
 
-  $.ajax({
-    type: 'POST',
-    url: '/api/friends',
-    data: dataForm,
-    success: function(response) {
-      console.log('response', response);
-    },
-    error: function(err) {}
-  });
+    $.ajax({
+      type: 'POST',
+      url: '/api/friends',
+      data: dataForm,
+      success: function(response) {
+        $('#friend-name').html(response.name);
+        $('#friend-photo').attr('src', response.photo);
+
+        $('#friend-modal').modal('toggle');
+      },
+      error: function(err) {}
+    });
+  } else {
+    alert('Some data is required!!');
+  }
 });
